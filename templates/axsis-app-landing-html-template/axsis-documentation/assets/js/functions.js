0acaa50800444d489c1419fd767978c63ffc9e6b
Derrick Ngigi <ngigid0@gmail.com>
2021-01-23 19:32:56 +0300

    Templates

---
+++ templates/axsis-app-landing-html-template/axsis-documentation/assets/js/functions.js
@@ -0,0 +1,11 @@
+(function ($){
+    'use strict';
+    jQuery(document).ready(function () {
+
+    	  $('.sidebar-option').on("click", function(){
+            $('body').toggleClass('sidebar-open');
+          })
+
+    });
+})(jQuery);
+
